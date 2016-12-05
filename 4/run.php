1c1fc62a86675a88968d228fead0a7905d7e4a93
Shane Mc Cormack <shanemcc@gmail.com>
2016-12-05 03:48:26 +0000

    Format debug output nicer

--- 4/run.php
+++ 4/run.php
@@ -18,7 +18,7 @@
 		});
 
 		$calcSum = implode('', array_slice(array_keys($letters), 0, 5));
-		return $calcSum == $checksum;
+		return [$calcSum == $checksum, $calcSum];
 	}
 
 	function decryptSectorName($name, $sector) {
@@ -41,17 +41,17 @@
 		preg_match('#([a-z\-]+)-([0-9]+)\[([a-z]{5})\]#i', $details, $m);
 		list($all, $name, $sector, $checksum) = $m;
 
-		$valid = isValidSector($name, $checksum);
-		debugOut('Sector ', $sector, ': ', $name, ' [', $checksum, '] => ', ($valid ? 'VALID' : 'BAD'), "\n");
+		list($valid, $realsum) = isValidSector($name, $checksum);
+		$decrypted = decryptSectorName($name, $sector);
+
+		debugOut(sprintf('Sector %3s: %60s [%s == %s] => %-5s {%s}', $sector, $name, $checksum, $realsum, ($valid ? 'VALID' : ' BAD'), $decrypted), "\n");
 
 		if ($valid) {
 			$part1 += $sector;
-
-			$decrypted = decryptSectorName($name, $sector);
-			debugOut("\t", $decrypted, "\n");
 			if ($decrypted == 'northpole object storage') { $part2 = $sector; }
 		}
 	}
 
+	debugOut("\n");
 	echo 'Part 1: ', $part1, "\n";
 	echo 'Part 2: ', $part2, "\n";
