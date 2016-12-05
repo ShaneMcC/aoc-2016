4a0a7a0835060b77cdb445b52f4d1758f88cc26f
Shane Mc Cormack <shanemcc@gmail.com>
2016-12-05 04:05:35 +0000

    Tidy day 4 a bit.

--- 4/run.php
+++ 4/run.php
@@ -3,36 +3,20 @@
 	require_once(dirname(__FILE__) . '/../common/common.php');
 	$input = getInputLines();
 
-	function isValidSector($name, $checksum) {
+	function getChecksum($name) {
 		$letters = [];
 		foreach (str_split(str_replace('-', '', $name)) as $bit) {
-			if (!isset($letters[$bit])) { $letters[$bit] = 0; }
-			$letters[$bit]++;
+			if (!isset($letters[$bit])) { $letters[$bit] = 1; } else { $letters[$bit]++; }
 		}
-
-		uksort($letters, function ($a, $b) use ($letters) {
-			if ($letters[$a] == $letters[$b]) {
-				return strcmp($a, $b);
-			}
-			return $letters[$b] - $letters[$a];
-		});
-
-		$calcSum = implode('', array_slice(array_keys($letters), 0, 5));
-		return [$calcSum == $checksum, $calcSum];
+		uksort($letters, function ($a, $b) use ($letters) { return ($letters[$a] == $letters[$b]) ? strcmp($a, $b) : $letters[$b] - $letters[$a]; });
+		return implode('', array_slice(array_keys($letters), 0, 5));
 	}
 
 	function decryptSectorName($name, $sector) {
-		$letters = str_split('abcdefghijklmnopqrstuvqxyz');
-
 		$name = str_split($name);
 		for ($i = 0; $i < count($name); $i++) {
-			if ($name[$i] == '-') {
-				$name[$i] = ' ';
-			} else {
-				$name[$i] = $letters[(array_search($name[$i], $letters) + $sector) % count($letters)];
-			}
+			$name[$i] = ($name[$i] == '-') ? ' ' : chr(((ord($name[$i]) + $sector - 97) % 26) + 97);
 		}
-
 		return implode('', $name);
 	}
 
@@ -41,7 +25,8 @@
 		preg_match('#([a-z\-]+)-([0-9]+)\[([a-z]{5})\]#i', $details, $m);
 		list($all, $name, $sector, $checksum) = $m;
 
-		list($valid, $realsum) = isValidSector($name, $checksum);
+		$realsum = getChecksum($name);
+		$valid = $realsum == $checksum;
 		$decrypted = decryptSectorName($name, $sector);
 
 		debugOut(sprintf('Sector %3s: %60s [%s == %s] => %-5s {%s}', $sector, $name, $checksum, $realsum, ($valid ? 'VALID' : ' BAD'), $decrypted), "\n");
