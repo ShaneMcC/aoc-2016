0e15edb60ebb0b6b251c2d81bf2c93dfb8a3a11f
Shane Mc Cormack <shanemcc@gmail.com>
2016-12-06 19:28:55 +0000

    Simplify a bit.

--- 6/run.php
+++ 6/run.php
@@ -5,18 +5,15 @@
 
 	$counts = [];
 	$part1 = $part2 = '';
-
 	foreach ($input as $line) {
-		$bits = str_split(trim($line));
-		for ($i = 0; $i < count($bits); $i++) {
-			$letter = $bits[$i];
-			if (!isset($counts[$i])) { $counts[$i] = []; }
+		for ($i = 0; $i < strlen($line); $i++) {
+			$letter = $line{$i};
 			if (!isset($counts[$i][$letter])) { $counts[$i][$letter] = 1; } else { $counts[$i][$letter]++; }
 		}
 	}
 
 	foreach ($counts as $i => $letters) {
-		uksort($letters, function ($a, $b) use ($letters) { return ($letters[$a] == $letters[$b]) ? strcmp($a, $b) : $letters[$b] - $letters[$a]; });
+		arsort($letters);
 		$keys = array_keys($letters);
 		$part1 .= array_shift($keys);
 		$part2 .= array_pop($keys);
