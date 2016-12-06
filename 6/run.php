e5db70adbffff81e83cb28ac459d437eee84fc79
Shane Mc Cormack <shanemcc@gmail.com>
2016-12-06 19:44:44 +0000

    Use built-ins

--- 6/run.php
+++ 6/run.php
@@ -3,20 +3,16 @@
 	require_once(dirname(__FILE__) . '/../common/common.php');
 	$input = getInputLines();
 
-	$counts = [];
+	$lines = [];
 	$part1 = $part2 = '';
-	foreach ($input as $line) {
-		for ($i = 0; $i < strlen($line); $i++) {
-			$letter = $line{$i};
-			if (!isset($counts[$i][$letter])) { $counts[$i][$letter] = 1; } else { $counts[$i][$letter]++; }
-		}
-	}
+	foreach ($input as $line) { $lines[] = str_split(trim($line)); }
 
-	foreach ($counts as $i => $letters) {
-		arsort($letters);
-		$keys = array_keys($letters);
-		$part1 .= array_shift($keys);
-		$part2 .= array_pop($keys);
+	for ($i = 0; $i < count($lines[0]); $i++) {
+		$keys = count_chars(implode('', array_column($lines, $i)), 1);
+		arsort($keys);
+		$keys = array_keys($keys);
+		$part1 .= chr(array_shift($keys));
+		$part2 .= chr(array_pop($keys));
 	}
 
 	echo 'Part 1: ', $part1, "\n";
