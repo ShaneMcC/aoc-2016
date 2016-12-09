07695f12c28a925491cc7495630e0f9563acab6a
Shane Mc Cormack <shanemcc@gmail.com>
2016-12-09 05:20:04 +0000

    Day 9, Part 2. Messy

--- 9/run.php
+++ 9/run.php
@@ -4,7 +4,7 @@
 	$input = getInputLines();
 
 
-	function decompress($line) {
+	function decompressV1($line) {
 		$newLine = '';
 		$brackets = FALSE;
 		$bracketsData = '';
@@ -25,9 +25,36 @@
 			}
 		}
 
-		echo $line, ' => ', trim($newLine), ' {', strlen($newLine), '}', "\n";
+		return $newLine;
+	}
+
+	function decompressV2Size($line) {
+		$count = 0;
+		$brackets = FALSE;
+		$bracketsData = '';
+		for ($i = 0; $i < strlen($line); $i++) {
+			if ($line{$i} == '(') {
+				$bracketsData = '';
+				$brackets = TRUE;
+			} else if ($line{$i} == ')') {
+				$brackets = FALSE;
+				preg_match('#([0-9]+)x([0-9]+)#', $bracketsData, $m);
+				list($all, $chars, $times) = $m;
+				$count += (decompressV2Size(substr($line, $i + 1, $chars)) * $times);
+				$i += $chars;
+			} else if ($brackets) {
+				$bracketsData .= $line{$i};
+			} else {
+				$count++;
+			}
+		}
+
+		return $count;
 	}
 
 	foreach ($input as $line) {
-		decompress($line);
+		// $newLine = decompressV2($line);
+		// echo $line, ' => ', trim($newLine), ' {', strlen($newLine), '}', "\n";
+		//
+		echo decompressV2Size($line);
 	}
