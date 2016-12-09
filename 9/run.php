c52a55c560bf67ac5381b6eeb57e2c6737b46ab3
Shane Mc Cormack <shanemcc@gmail.com>
2016-12-09 06:05:12 +0000

    Get rid of code that outputs decompressed data.
    It just makes things uglier for no reason given the size of part 2...

--- 9/run.php
+++ 9/run.php
@@ -3,8 +3,7 @@
 	require_once(dirname(__FILE__) . '/../common/common.php');
 	$input = getInputLine();
 
-	function decompress($line, $version = 1, $sizeOnly = true) {
-		$newLine = '';
+	function decompress($line, $version = 1) {
 		$count = 0;
 		for ($i = 0; $i < strlen($line); $i++) {
 			if ($line{$i} == '(') {
@@ -14,27 +13,18 @@
 
 				preg_match('#([0-9]+)x([0-9]+)#', $bracketsData, $m);
 				list($all, $chars, $times) = $m;
-				if ($sizeOnly) {
-					$compressedSize = ($version == 1) ? strlen(substr($line, $i + 1, $chars)) : decompress(substr($line, $i + 1, $chars), $version, true);
-					$count += $compressedSize * $times;
-				} else {
-					$bit = ($version == 1) ? substr($line, $i + 1, $chars) : decompress(substr($line, $i + 1, $chars), $version, false);
-					$newLine .= str_repeat($bit, $times);
-				}
+
+				$next = substr($line, $i + 1, $chars);
+				$compressedSize = ($version == 1) ? strlen($next) : decompress($next, $version);
+				$count += $compressedSize * $times;
 				$i += $chars;
 			} else {
-				if ($sizeOnly) { $count++; } else { $newLine .= $line{$i}; }
+				$count++;
 			}
 		}
 
-		return ($sizeOnly) ? $count : $newLine;
+		return $count;
 	}
 
-	if (isDebug()) {
-		echo 'Input: ', $input, "\n";
-		echo 'Part 1: {', decompress($input, 1, true), '} => ', decompress($input, 1, false), "\n";
-		echo 'Part 2: {', decompress($input, 2, true), '} => ', decompress($input, 2, false), "\n";
-	} else {
-		echo 'Part 1: ', decompress($input, 1, true), "\n";
-		echo 'Part 2: ', decompress($input, 2, true), "\n";
-	}
+	echo 'Part 1: ', decompress($input, 1), "\n";
+	echo 'Part 2: ', decompress($input, 2), "\n";
