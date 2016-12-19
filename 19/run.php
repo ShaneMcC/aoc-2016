0f8b96b27d6b44f13ced4467620a484b82b06ff1
Shane Mc Cormack <shanemcc@gmail.com>
2016-12-19 23:58:21 +0000

    Yet more minor part 2 math changes.

--- 19/run.php
+++ 19/run.php
@@ -19,23 +19,17 @@
 	// 2 4 10 28 82 244 730 ...
 	// (or, the powers of 3 (1 3 9 27 81) + 1)
 	//
-	// From there until half way to the next number in the sequence, the elves
-	// sequentially get the present from 1..2..3..4 etc. After the mid point,
-	// every other elf gets it...
-	//
-	// I'm sure there is a formula here somewhere, I just don't know it yet...
+	// From there until (3^n * 2) the elves sequentially get the present from
+	// 1..2..3..4 etc. After wards, every other elf from there onwards gets it.
 	function doPart2($input) {
 		$i = pow(3, floor(log($input, 3)));
-		$nextI = ($i * 3) - 2;
-
-		$mid = ($i + $nextI) / 2;
 
 		if ($input == $i) {
 			return $i;
-		} else if ($input <= $mid) {
+		} else if ($input <= ($i * 2)) {
 			return ($input - $i);
 		} else {
-			return (2 * $input) - $mid - $i - 1;
+			return (2 * $input) - (3 * $i);
 		}
 	}
 
