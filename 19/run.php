7f36a30369cf62de23baabfdc9cea3566dece6e8
Shane Mc Cormack <shanemcc@gmail.com>
2016-12-19 22:18:05 +0000

    Simply massively.

--- 19/run.php
+++ 19/run.php
@@ -6,16 +6,8 @@
 	// If an elf count is a power of 2, elf #1 gets the presents.
 	// For every number above that, the next odd elf gets it.
 	function doPart1($input) {
-		// Find previous power of 2
-		$pow = pow(2, floor(log($input, 2)));
-
-		// Count up from there.
-		$ans = 1;
-		for ($i = $pow; $i < $input; $i++) { $ans += 2; }
-
-		return $ans;
+		return 1 + (2 * ($input - pow(2, floor(log($input, 2)))));
 	}
 
 	$part1 = doPart1($input);
 	echo 'Part 1: ', $part1, "\n";
-
