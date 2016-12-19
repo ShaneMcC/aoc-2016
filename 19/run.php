65c28470a2e7a4d4f0fad57e67914f7fba02d875
Shane Mc Cormack <shanemcc@gmail.com>
2016-12-19 23:29:38 +0000

    Slightly nicer maths.

--- 19/run.php
+++ 19/run.php
@@ -15,6 +15,7 @@
 	// Elf 1 gets the present every time there is a number of elves
 	// equal to the sequence of "(x*3) - 2"
 	// 2 4 10 28 82 244 730 ...
+	// (or, the powers of 3 (1 3 9 27 81) + 1)
 	//
 	// From there until half way to the next number in the sequence, the elves
 	// sequentially get the present from 1..2..3..4 etc. After the mid point,
@@ -22,22 +23,15 @@
 	//
 	// I'm sure there is a formula here somewhere, I just don't know it yet...
 	function doPart2($input) {
-		$i = 2;
-		while (true) {
-			$j = ($i * 3) - 2;
-			if ($j <= $input) { $i = $j; } else { break; }
-		}
+		$i = pow(3, floor(log($input - 1, 3))) + 1;
 		$nextI = ($i * 3) - 2;
 
-		$mid = $i + (($nextI - $i) / 2);
+		$mid = ($i + $nextI) / 2;
 
 		if ($input < $mid) {
 			return ($input - $i) + 1;
 		} else {
-			$midNum = ($mid - $i) + 1;
-			$loc = ($input - $mid) + 1;
-
-			return (2 * $loc) + $midNum - 1;
+			return (2 * $input) - $mid - $i + 2;
 		}
 	}
 
