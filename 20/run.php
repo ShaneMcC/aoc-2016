85a6b7b38831cc098e8e1dd19937927874c1ea2a
Shane Mc Cormack <shanemcc@gmail.com>
2016-12-20 19:53:25 +0000

    Minor chagnes to allow block lists that don't block the start/end IPs.

--- 20/run.php
+++ 20/run.php
@@ -12,19 +12,23 @@
 	uasort($blocked, function($a, $b) { return ($a['start'] == $b['start']) ? $a['end'] - $b['end'] : $a['start'] - $b['start']; });
 
 	function getAnswers($blocked) {
-		$min = $allowed = $thisStart = $nextEnd = 0;
+		$allowed = 0;
+		$min = $thisStart = $nextEnd = -1;
 
 		foreach ($blocked as $block) {
 			debugOut('Blocked: ', $block['start'], ' => ', $block['end'], "\n");
 
-			if ($block['start'] > $nextEnd) {
-				if ($min == 0) { $min = $nextEnd; }
-				$allowed += $block['start'] - $nextEnd;
+			if ($block['start'] > $nextEnd + 1) {
+				if ($min == -1) { $min = ($nextEnd + 1); }
+				$allowed += $block['start'] - ($nextEnd + 1);
 			}
 
 			$thisStart = min($thisStart, $block['start']);
-			$nextEnd = max($nextEnd, $block['end'] + 1);
+			$nextEnd = max($nextEnd, $block['end']);
 		}
+
+		$max = isTest() ? 9 : 4294967295;
+		if ($nextEnd <= $max) { $allowed += $max - $nextEnd; }
 
 		return [$min, $allowed];
 	}
